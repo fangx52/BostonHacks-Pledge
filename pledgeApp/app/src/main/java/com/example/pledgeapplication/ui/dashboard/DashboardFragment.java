2197c74b6774a6e94a884706a08e6bd13fba8f87
AndrewLiu12 <liua@bu.edu>
2019-11-17 02:59:20 -0500

    feed

--- pledgeApp/app/src/main/java/com/example/pledgeapplication/ui/dashboard/DashboardFragment.java
+++ pledgeApp/app/src/main/java/com/example/pledgeapplication/ui/dashboard/DashboardFragment.java
@@ -6,30 +6,64 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.pledgeapplication.R;
 
 public class DashboardFragment extends Fragment {
+    String[] strings = {"Your Friend Has Completed So-And-So Activity",
+            "Your Friend Has Pledged So-And-So Activity",
+            "Your Friend Has Volunteered So-And-So Activity",
+            "Your Friend Has Completed So-And-So Activity"};
 
-    private DashboardViewModel dashboardViewModel;
+    public DashboardFragment() {}
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView rv = new RecyclerView(getContext());
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(new SimpleRVAdapter(strings));
+        return rv;
+    }
+
+    /**
+     * A Simple Adapter for the RecyclerView
+     */
+    public class SimpleRVAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
+        private String[] dataSource;
+        public SimpleRVAdapter(String[] dataArgs){
+            dataSource = dataArgs;
+        }
+
+        @Override
+        public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = new TextView(parent.getContext());
+            SimpleViewHolder viewHolder = new SimpleViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(SimpleViewHolder holder, int position) {
+            holder.textView.setTextSize(24);
+            holder.textView.setTextIsSelectable(true);
+            holder.textView.setText(dataSource[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSource.length;
+        }
+    }
+
+    /**
+     * A Simple ViewHolder for the RecyclerView
+     */
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
     }
 }
---
+++ pledgeApp/app/src/main/java/com/example/pledgeapplication/ui/dashboard/Dashboard_list_adaptor.java
@@ -0,0 +1,117 @@
+package com.example.pledgeapplication.ui.dashboard;
+
+import android.content.Context;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.pledgeapplication.R;
+
+import java.util.LinkedList;
+
+public class Dashboard_list_adaptor extends
+        RecyclerView.Adapter<Dashboard_list_adaptor.DashboardViewHolder>{
+
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    class DashboardViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        public final TextView wordItemView;
+        final Dashboard_list_adaptor mAdapter;
+
+        /**
+         * Creates a new custom view holder to hold the view to display in
+         * the RecyclerView.
+         *
+         * @param itemView The view in which to display the data.
+         * @param adapter The adapter that manages the the data and views
+         *                for the RecyclerView.
+         */
+        public DashboardViewHolder(View itemView, Dashboard_list_adaptor adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public Dashboard_list_adaptor(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    /**
+     * Called when RecyclerView needs a new ViewHolder of the given type to
+     * represent an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can
+     * represent the items of the given type. You can either create a new View
+     * manually or inflate it from an XML layout file.
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * onBindViewHolder(ViewHolder, int, List). Since it will be reused to
+     * display different items in the data set, it is a good idea to cache
+     * references to sub views of the View to avoid unnecessary findViewById()
+     * calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after
+     *                 it is bound to an adapter position.
+     * @param viewType The view type of the new View. @return A new ViewHolder
+     *                 that holds a View of the given view type.
+     */
+    @Override
+    public Dashboard_list_adaptor.DashboardViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+        // Inflate an item view.
+        View mItemView = mInflater.inflate(
+                R.layout.fragment_dashboard, parent, false);
+        return new DashboardViewHolder(mItemView, this);
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     * This method should update the contents of the ViewHolder.itemView to
+     * reflect the item at the given position.
+     *
+     * @param holder   The ViewHolder which should be updated to represent
+     *                 the contents of the item at the given position in the
+     *                 data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(Dashboard_list_adaptor.DashboardViewHolder holder,
+                                 int position) {
+        // Retrieve the data for that position.
+        String mCurrent = mWordList.get(position);
+        // Add the data to the view holder.
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+}
