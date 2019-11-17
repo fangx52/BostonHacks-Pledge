7b75b8bc3c7944568efe606088d01f87d6edf631
Xuanyu Fang <fangx@bu.edu>
2019-11-17 10:19:13 -0600

    finally

--- pledgeApp/app/src/main/java/com/example/pledgeapplication/ui/notifications/DiscoverFragment.java
+++ pledgeApp/app/src/main/java/com/example/pledgeapplication/ui/notifications/DiscoverFragment.java
@@ -53,12 +53,18 @@ public class DiscoverFragment extends Fragment {
     // initializes the data - dashboard arrays
     public ArrayList<DiscoverItem> initDiscoverArray() {
         ArrayList<DiscoverItem> allDiscover = new ArrayList<DiscoverItem>();
-        DiscoverItem dd1 = new DiscoverItem("Elton rode the bus instead of driving to work all month.",
-                "Other", "Other", "Other", "Other");
-        DiscoverItem dd2 = new DiscoverItem("Andrew didn't use plastic cups for a week.",
-                "Other", "Other", "Other", "Other");
+        DiscoverItem dd1 = new DiscoverItem("Boston, MA",
+                "Sustainability Festival", "Come out to the Boston Sustainability Festival to learn about how to be sustainable.", "11.28.19", "bostonsustainabilty.org");
+        DiscoverItem dd2 = new DiscoverItem("Boston, MA",
+                "Women in Green: Culture of Courage", "Women are driving the sustainability of the built environment. We are tasked with the most important mission of all: healing the earth.", "12.3.19", "https://www.eventbrite.com/e/women-in-green-culture-of-courage-tickets-77516753697?aff=ebdssbdestsearch");
+        DiscoverItem dd3 = new DiscoverItem("Online",
+                "#TeamTrees", "Help us plant 20 million trees around the globe by January 1st 2020.", "Now until 2020", "teamtrees.org");
+        DiscoverItem dd4 = new DiscoverItem("Boston, MA",
+                "Boston Sustainability Breakfast", "Join us every month for Net Impact Boston's informal breakfast meetup of sustainability professionals for networking, discussion, and moral support. ", "11.20.19", "https://www.eventbrite.com/e/boston-sustainability-breakfast-tickets-72997402195");
         allDiscover.add(dd1);
         allDiscover.add(dd2);
+        allDiscover.add(dd3);
+        allDiscover.add(dd4);
         return allDiscover;
     }
 
